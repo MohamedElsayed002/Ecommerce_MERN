14062a73b5591f6f60e96e83459a6efa8b84019b
Mohamed Elsayed Ali <99518714+MohamedElsayed002@users.noreply.github.com>
2024-03-26 05:49:41 +0200

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 
 
 import mongoose from "mongoose";
-
+// env file 
 export function dbConnection () {
     mongoose.connect(process.env.MONGO_URL)
         .then(() => console.log('database connection established'))
